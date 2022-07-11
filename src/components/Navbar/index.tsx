1cfb26b27b41fba7cff065ce4d33236088f96dd8
Anton Zavalniuk <zavalniuk.dev@gmail.com>
2022-07-11 10:08:30 +0200

    connect theme  provider

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,22 @@
+import { useTheme as useNextTheme } from "next-themes";
+import { Switch, useTheme } from "@nextui-org/react";
+
 import { useIntl } from "../../hooks/useIntl";
 import LanguageSelect from "./LanguageSelect";
 
 const Navbar = () => {
+  const { setTheme } = useNextTheme();
+  const { isDark, type } = useTheme();
   const { intl } = useIntl();
 
   return (
-    <header style={{ height: "80px", width: "100%" }}>
+    <header style={{ width: "100%", background: "teal" }}>
       <p>{intl("app.banner")}</p>
       <nav>{intl("app.header")}</nav>
+      <Switch
+        checked={isDark}
+        onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
+      />
       <LanguageSelect />
     </header>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,41 @@
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
-import { NextUIProvider } from "@nextui-org/react";
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 import { IntlProvider } from "../context/intlContext";
 
+const lightTheme = createTheme({
+  type: "light",
+  // theme: {
+  //   colors: {...}, // optional
+  // }
+});
+
+const darkTheme = createTheme({
+  type: "dark",
+  // theme: {
+  //   colors: {...}, // optional
+  // }
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <IntlProvider>
-        <Component {...pageProps} />
-      </IntlProvider>
-    </NextUIProvider>
+    <NextThemesProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
+      }}
+    >
+      <NextUIProvider>
+        <IntlProvider>
+          <Component {...pageProps} />
+        </IntlProvider>
+      </NextUIProvider>
+    </NextThemesProvider>
   );
 }
 
